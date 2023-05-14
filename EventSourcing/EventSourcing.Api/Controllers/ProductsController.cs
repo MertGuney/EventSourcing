c158ba6ad7aa9b1f9cd9e1335a319c42a2d00dd9
MertGuney <guneeymert@gmail.com>
2023-05-14 14:23:28 +0300

    Product controller created

---
+++ EventSourcing/EventSourcing.Api/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcing.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto createProduct)
+        {
+            await _mediator.Send(new CreateProductCommand() { CreateProduct = createProduct });
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChangeName(ChangeProductNameDto changeProductName)
+        {
+            await _mediator.Send(new ChangeProductNameCommand() { ChangeProductName = changeProductName });
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChangePrice(ChangeProductPriceDto changeProductPrice)
+        {
+            await _mediator.Send(new ChangeProductPriceCommand() { ChangeProductPrice = changeProductPrice });
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteProductCommand() { Id = id });
+            return NoContent();
+        }
+    }
+}
