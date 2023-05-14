c4cfefca84d02d683dad02f784832f5036b72205
MertGuney <guneeymert@gmail.com>
2023-05-14 13:01:24 +0300

    MediatR added and unneeded class removed

--- EventSourcing/EventSourcing.Api/Program.cs
+++ EventSourcing/EventSourcing.Api/Program.cs
@@ -1,6 +1,9 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
