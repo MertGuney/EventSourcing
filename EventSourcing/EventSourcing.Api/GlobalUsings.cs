92f5da3db6c92243338248ed7bff74011646bcf4
MertGuney <guneeymert@gmail.com>
2023-05-14 13:49:58 +0300

    .

--- EventSourcing/EventSourcing.Api/GlobalUsings.cs
+++ EventSourcing/EventSourcing.Api/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using EventSourcing.Api.DTOs.Products;
 global using EventSourcing.Api.Events.Common.Interfaces;
+global using EventSourcing.Api.Extensions;
+global using EventStore.ClientAPI;
 global using MediatR;
 global using System.Reflection;
--- EventSourcing/EventSourcing.Api/Program.cs
+++ EventSourcing/EventSourcing.Api/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
+builder.Services.AddEventStore(builder.Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
